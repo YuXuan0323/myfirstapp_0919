233b2e204351c01f08541dee2cf81b00af52c199
YuXuan0323 <113335632+YuXuan0323@users.noreply.github.com>
2022-09-26 14:33:42 +0800

    master to main

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 建立appTitle和hiFlutter物件
+  var appTitle = Text('我的第一個Flutter App'),
+      hiFlutter = Text("Hi, Flutter.",
+        style: TextStyle(fontSize: 30),
+      );
+
+  // 建立appBody物件
+  var appBody = Center(
+    child: hiFlutter,
+  );
+
+  // 建立appBar物件
+  var appBar = AppBar(
+    title: appTitle,
+    backgroundColor: Color(0xff0000ff),
+  );
+
+  // 建立app物件
+  var app = MaterialApp(
+    home: Scaffold(
+        appBar: appBar,
+        body: appBody,
+        backgroundColor: Colors.yellow
+    ),
+  );
+
+  runApp(app);
+}
